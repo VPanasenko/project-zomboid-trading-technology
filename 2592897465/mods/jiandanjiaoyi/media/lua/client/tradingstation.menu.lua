299b6449c2d0bf8a0ab4434ace17e365147b9b6a
VPanasenko <panasenko.victor@gmail.com>
2021-11-21 14:32:19 +0300

    More recipes, fixes and attempts to add custom submenu

--- 2592897465/mods/jiandanjiaoyi/media/lua/client/tradingstation.menu.lua
+++ 2592897465/mods/jiandanjiaoyi/media/lua/client/tradingstation.menu.lua
@@ -1,46 +1,3 @@
--- function ISInventoryPaneContextMenu.addDynamicalContextMenu(selectedItem, context, recipeList, player, containerList)
-
---     if (esBundler.isGoodToPack(selectedItem)) then
---         local rawStack = esCommonBundler:getItemStackByFullType(selectedItem:getFullType(), selectedItem:getContainer());
---         local stack = LuaList:new();
-
---         for i = 0, rawStack:size() - 1 do
---             local item = rawStack:get(i);
---             if (esBundler.isGoodToPack(item)) then
---                 stack:add(item);
---             end
---         end
-
---         -- bundle Volumes
---         local itemW = selectedItem:getActualWeight();
---         local maxW = esBundler.getMaxSheetRopes();
---         local units = { 1, 5, 10, 25, 50, 100, 150, 250, 500 };
---         local bundlerSubMenu;
-
---         if (stack:size() > 0 and maxW >= itemW) then bundlerSubMenu = context:getNew(context); end
-
---         for u = 1, #units do
---             if (stack:size() >= units[u] and maxW >= itemW * units[u]) then
---                 local stackRecipe = esBundler.getBundleItems(selectedItem, stack, units[u], 'ESBundler.esBag5')
---                 bundlerSubMenu:addOption(stackRecipe[1], stackRecipe[2], esBundler.deflate, stackRecipe[3], stackRecipe[4], units[u]);
---             else
---                 break;
---             end
---         end
-
---         if (bundlerSubMenu and #bundlerSubMenu.options > 0) then
---             context:addSubMenu(context:addOption('Bundler:'), bundlerSubMenu);
---         end
---     end
-
---     if (selectedItem:getFullType() == 'ESBundler.esBag5') then
---         local recipeName = 'Unpack ' .. selectedItem:getDisplayName();
---         context:addOption(recipeName, selectedItem, esBundler.inflate);
---     end
-
---     baseISInventoryPaneContextMenu(selectedItem, context, recipeList, player, containerList);
--- end
-
 function tradingstation.isGoodForTradingStation(selectedItem)
     if  selectedItem:getType() == "KeyRing" or
         selectedItem:isEquipped()
@@ -63,80 +20,67 @@ function ISInventoryPaneContextMenu.addDynamicalContextMenu(selectedItem, contex
 
     if (tradingstation.isGoodForTradingStation(selectedItem)) then
         if (recipeList:size() > 0) then
+            local tradingStationSubMenu = context:getNew(context);
+            local tradingStationSubMenu_buy = context:getNew(context);
+            local tradingStationSubMenu_sell = context:getNew(context);
+            local tradingStationSubMenu_make = context:getNew(context);
+            local tradingStationSubMenu_lottery = context:getNew(context);
+            local tradingStationSubMenu_refine = context:getNew(context);
+            local tradingStationSubMenu_inject = context:getNew(context);
+            local tradingStationSubMenu_other = context:getNew(context);
             for i=0,recipeList:size() -1 do
                 local recipe = recipeList:get(i);
-                if (recipe:getName().find(tradingstation_category_buy) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_buy")), context);
-                }
-                elseif (recipe:getName().find(tradingstation_category_sell) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_sell")), context);
-                }
-                elseif (recipe:getName().find(tradingstation_category_make) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_make")), context);
-                }
-                elseif (recipe:getName().find(tradingstation_category_lottery) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_lottery")), context);
-                }
-                elseif (recipe:getName().find(tradingstation_category_refine) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_refine")), context);
-                }
-                elseif (recipe:getName().find(tradingstation_category_inject) ~= nil){
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_inject")), context);
-                }
-                else{
-                    context:addSubMenu(context:addOption(getText("ContextMenu_category_other")), context);
-                }
+                local option = nil;
+                local subMenuCraft = nil;
+                subMenuCraft = context:getNew(context);
+                option = context:addOption(recipe:getName(), selectedItem, nil);
+                context:addSubMenu(option, subMenuCraft);
+                local subOption = subMenuCraft:addOption(getText("ContextMenu_One"), selectedItem, ISInventoryPaneContextMenu.OnCraft, recipe, player, false);
+
+                if (recipe:getName():find(tradingstation_category_buy) ~= nil) then
+                    tradingStationSubMenu_buy:addOption(recipe:getName(), selectedItem, nil);
+                elseif (recipe:getName():find(tradingstation_category_sell) ~= nil) then
+                    tradingStationSubMenu_sell:addOption(recipe:getName(), selectedItem, nil);
+                elseif (recipe:getName():find(tradingstation_category_make) ~= nil) then
+                    tradingStationSubMenu_make:addOption(recipe:getName(), selectedItem, nil);
+                elseif (recipe:getName():find(tradingstation_category_lottery) ~= nil) then
+                    tradingStationSubMenu_lottery:addOption(recipe:getName(), selectedItem, nil);
+                elseif (recipe:getName():find(tradingstation_category_refine) ~= nil) then
+                    tradingStationSubMenu_refine:addOption(recipe:getName(), selectedItem, nil);
+                elseif (recipe:getName():find(tradingstation_category_inject) ~= nil) then
+                    tradingStationSubMenu_inject:addOption(recipe:getName(), selectedItem, nil);
+                else
+                    tradingStationSubMenu_other:addOption(recipe:getName(), selectedItem, nil);
+                end
             end
-        end
 
-        if (selectedItem:getDisplayName() == "Pager"){
-            context:addSubMenu(context:addOption("Pager:"), context);
-        }
+            if (tradingStationSubMenu_buy and #tradingStationSubMenu_buy.options > 0) then
+                local tradingStationSubMenuBuyOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_buy"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuBuyOption, tradingStationSubMenu_buy);
+            elseif (tradingStationSubMenu_sell and #tradingStationSubMenu_sell.options > 0) then
+                local tradingStationSubMenuSellOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_sell"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuSellOption, tradingStationSubMenu_sell);
+            elseif (tradingStationSubMenu_make and #tradingStationSubMenu_make.options > 0) then
+                local tradingStationSubMenuMakeOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_make"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuMakeOption, tradingStationSubMenu_make);                
+            elseif (tradingStationSubMenu_lottery and #tradingStationSubMenu_lottery.options > 0) then
+                local tradingStationSubMenuLotteryOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_lottery"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuLotteryOption, tradingStationSubMenu_lottery);
+            elseif (tradingStationSubMenu_refine and #tradingStationSubMenu_refine.options > 0) then
+                local tradingStationSubMenuRefineOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_refine"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuRefineOption, tradingStationSubMenu_refine);
+            elseif (tradingStationSubMenu_inject and #tradingStationSubMenu_inject.options > 0) then
+                local tradingStationSubMenuInjectOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_inject"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuInjectOption, tradingStationSubMenu_inject);
+            else
+                local tradingStationSubMenuBuyOtherOption = tradingStationSubMenu:addOption(getText("ContextMenu_category_other"));
+                tradingStationSubMenu:addSubMenu(tradingStationSubMenuBuyOtherOption, tradingStationSubMenu_other);                                                                
+            end
 
-        if (context and #context.options > 0) then
-            context:addSubMenu(context:addOption(getText("ContextMenu_category")), context);
+            local tradingStationSubMenuOption = context:addOption(getText("ContextMenu_category"));
+            context:addSubMenu(tradingStationSubMenuOption, tradingStationSubMenu);
         end
     end
 
     baseISInventoryPaneContextMenu(selectedItem, context, recipeList, player, containerList);
 end
-
--- TradingStation.Event = TradingStation.Event or {
---     currentPlayerId = nil,
---     currentPlayer = nil,
--- };
-
--- function TradingStation.Event.onRespawn(playerId, player)
---     TradingStation.Event.currentPlayer = player or getPlayer();
---     TradingStation.Event.currentPlayerId = TradingStation.Event.currentPlayer:getID();
--- end
-
--- function TradingStation.Event:isFiredByPlayer(player)
---     return TradingStation.Event.currentPlayerId == player:getID();
--- end
-
--- function TradingStation.Event.onCreateContextMenu(player, contextMenu, worldobjects, test)
---     player = getSpecificPlayer(player);
-
---     if TradingStation.Event:isFiredByPlayer(player)
---     then
---         local btMenu = Bikinitools:addContextMenuSubMenu(contextMenu,
---             "TradingStation", nil, nil, {
---                 icon = true
---             }
---         );
-
---         Bikinitools.Options.createConfigContextMenu(btMenu, player);
---     end
--- end
-
--- function TradingStation.Event.initialize()
---     TradingStation.Event.onRespawn();
-
---     --Events.OnPlayerDeath.Add(Bikinitools.Event.onDeath);
---     Events.OnCreatePlayer.Add(TradingStation.Event.onRespawn);
-
---     Events.OnFillWorldObjectContextMenu.Add(TradingStation.Event.onCreateContextMenu);
--- end
-
--- Events.OnGameStart.Add(TradingStation.Event.initialize);
